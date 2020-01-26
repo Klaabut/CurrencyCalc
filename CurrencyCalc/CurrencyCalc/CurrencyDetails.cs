5b94f4eb906ff74321d88987f0164af927fb84cc
Klaabut <54986407+Klaabut@users.noreply.github.com>
2020-01-26 14:55:02 +0200

    1st commit

---
+++ CurrencyCalc/CurrencyCalc/CurrencyDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyCalc
+{
+    class CurrencyDetails
+    {
+        public string Base { get; set; }
+        public string Date { get; set; }
+        public string CAD { get; set; }
+
+        public List<CurrencyDetails> rates { get; set; }
+        
+    }
+}
---
+++ CurrencyCalc/CurrencyCalc/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Nancy;
+
+namespace CurrencyCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                    Console.WriteLine("What is the base currency you would like to calculate from?");
+                    string userFrom = Console.ReadLine();
+
+                    Console.WriteLine("What is the currency you would like to calculate to?");
+                    string userTo = Console.ReadLine();
+                    Console.WriteLine("Write the amount: ");
+                    double userAmount = int.Parse(Console.ReadLine());*/
+
+
+            string url = "https://api.exchangeratesapi.io/latest";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                var currencyDetails = JsonConvert.DeserializeObject<List<CurrencyDetails>>(response);
+                Console.WriteLine(currencyDetails[0].Base);
+                Console.WriteLine(currencyDetails[0].Date);
+                Console.WriteLine(currencyDetails[0].rates[0].CAD);
+                
+            }
+        }
+    }
+}
